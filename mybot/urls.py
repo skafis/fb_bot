935d342c4593b15b06c7a2ad7ccfae28cee14725
skafis <franciskipchumba5@gmail.com>
2016-06-28 20:42:56 +0300

    add random url and views to handle it

--- mybot/urls.py
+++ mybot/urls.py
@@ -1,2 +1,5 @@
 from django.conf.urls import include, url
-urlpatterns = []
+from . views import mybotview
+urlpatterns = [
+	url(r'^c7af68f7798531824d2dcfca43e9ff37bf8d781f41abb27c77/?$', mybotview.as_view())
+]
--- mybot/views.py
+++ mybot/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from django.http.response import HttpResponse
 
 # Create your views here.
+class mybotview (generic.View):
+	def get(self, request, *args, **kwargs):
+		return HttpResponse ("Hello world! MF")
+		
